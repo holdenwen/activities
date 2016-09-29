e6c0cea78447282f8dcfde717582ce31e7fbd226
timtimwen <timwenchina@163.com>
2016-09-29 15:50:23 +0800

    增加运行浏览器环境，顺便测试ssh是否修改成功

--- build/karma.unit.config.js
+++ build/karma.unit.config.js
@@ -2,6 +2,7 @@ import base from './karma.base.config.js'
 
 module.exports = function(config) {
     config.set(Object.assign(base, {
-        framework: ['jasmine']
+        framework: ['jasmine'],
+        browsers: ['phantomjs']
     }));
 };
